67f4645a0c5a8d0c3544e252a6d155d1c7cd9eaf
Vukašin Jekić <vuk.jekic@gmail.com>
2019-07-15 10:42:24 +0200

    rotacija modela, dugmici dole dodati

---
+++ Assets/ModelRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotation : MonoBehaviour
+{
+    public GameObject centar;
+    public float speed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //BoxCollider collider = GetComponent<BoxCollider>();
+                transform.RotateAround(centar.transform.position, Vector3.up * speed * Time.deltaTime, 1f);
+                //transform.RotateAroundLocal(Vector3.up, 10f);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.RotateAround(centar.transform.position, Vector3.up * speed * Time.deltaTime * -1, 1f);
+            }
+        }
+       
+    }
+}
